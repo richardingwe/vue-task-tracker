f2b657b042190df7a4502b3123b1a0efe96b9063
Richard Ingwe <richardingwe5@gmail.com>
2021-03-14 23:25:12 +0100

    corrected a warning  bug

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,8 @@
 <template>
-    <div :key="task.id" v-for="task in tasks">
-        <Task :task="task" />
+    <div>
+        <div :key="task.id" v-for="task in tasks">
+            <Task :task="task" />
+        </div>
     </div>
 </template>
 
