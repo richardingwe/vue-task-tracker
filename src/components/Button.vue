232e0f18445472a6da0f286836bd495c84d7e046
Richard Ingwe <richardingwe5@gmail.com>
2021-03-14 22:35:43 +0100

    added the button component

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="onclick()" :style="{background: color}" class="btn">
+        {{text}}
+    </button>
+</template>
+
+<script>
+    export default {
+        name: "Button",
+        props: {
+            text: String,
+            color: String
+        },
+        methods: {
+            onclick() {
+                alert("click")
+            }
+        }
+    }
+</script>
