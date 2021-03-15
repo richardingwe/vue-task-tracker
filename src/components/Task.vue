2b07d52867c1c59ff7184c5d5925950f1000304c
Richard Ingwe <richardingwe5@gmail.com>
2021-03-15 21:28:20 +0100

    created the deleteTask method

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,6 +1,6 @@
 <template>
     <div :class="[task.reminder ? 'reminder' : '', 'task']">
-        <h3>{{task.text}} <i class="fas fa-times"></i> </h3>
+        <h3>{{task.text}} <i @click="onDelete(task.id)" class="fas fa-times"></i> </h3>
         <p>{{task.day}}</p>
     </div>
 </template>
@@ -11,6 +11,11 @@
         props: {
             task: Object,
         },
+        methods: {
+            onDelete(id) {
+                this.$emit("delete-task", id)
+            }
+        }
     }
 </script>
 
