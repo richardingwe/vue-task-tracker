935b336fbc99b1195ffa27364149ce3d1da014cb
Richard Ingwe <richardingwe5@gmail.com>
2021-03-15 23:57:21 +0100

    added a proxy in vue.config.js

--- src/App.vue
+++ src/App.vue
@@ -42,14 +42,14 @@
         {...task, reminder: !task.reminder} : task)
       },
       async fetchTasks() {
-        const res = await fetch("http://localhost:5000/tasks");
+        const res = await fetch("api/tasks");
 
         const data = await res.json();
 
         return data;
       },
       async fetchTask(id) {
-        const res = await fetch(`http://localhost:5000/tasks/${id}`);
+        const res = await fetch(`api/tasks/${id}`);
 
         const data = await res.json();
 
