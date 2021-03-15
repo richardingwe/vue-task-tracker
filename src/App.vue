0c0ffc3310d7cb0db459563d45b7daa8864f202e
Richard Ingwe <richardingwe5@gmail.com>
2021-03-15 23:25:31 +0100

    added the fetch task method

--- src/App.vue
+++ src/App.vue
@@ -47,6 +47,13 @@
         const data = await res.json();
 
         return data;
+      },
+      async fetchTask(id) {
+        const res = await fetch(`http://localhost:5000/tasks/${id}`);
+
+        const data = await res.json();
+
+        return data;
       }
     },
     async created() {
