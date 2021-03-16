f608068ecc34a80f775d65e737d50e5b4e4a1687
Richard Ingwe <richardingwe5@gmail.com>
2021-03-16 09:01:46 +0100

    updated the toggle method to work with the json server

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,8 @@
         tasks: [],
         showAddTask: false,
       }
-    },methods: {
+    },
+    methods: {
       toggleAddTask() {
         this.showAddTask = !this.showAddTask
       },
@@ -42,14 +43,32 @@
 
         this.tasks = [...this.tasks, data]
       },
-      deleteTask(id) {
+      async deleteTask(id) {
         if(confirm("Are you sure?")) {
-          this.tasks = this.tasks.filter(task => task.id !== id)
+
+          const res = await fetch(`api/tasks/${id}`, {
+            method: 'DELETE'
+          })
+          res.status === 200 ? (
+            this.tasks = this.tasks.filter(task => task.id !== id)
+          ) : alert('Error deleting task')
         }
       },
-      toggleReminder(id) {
-        this.tasks = this.tasks.map(task => task.id === id ? 
-        {...task, reminder: !task.reminder} : task)
+      async toggleReminder(id) {
+        const taskToToggle = await this.fetchTask(id)
+        const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+        const res = await fetch(`api/tasks/${id}`, {
+            method: 'PUT',
+            headers: {
+              "Content-type": "application/json"
+            },
+            body: JSON.stringify(updTask),
+          })
+          
+          const data = await res.json();
+          
+          this.tasks = this.tasks.map(task => task.id === id ? 
+          {...task, reminder: data.reminder} : task)
       },
       async fetchTasks() {
         const res = await fetch("api/tasks");
